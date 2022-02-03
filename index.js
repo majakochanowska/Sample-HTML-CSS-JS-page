f4aa90a1e9888fa1c5b780756d178032918d036b
Maja Kochanowska <maja.kochanowska@mayeryn.com>
2022-02-03 21:23:56 +0100

    hamburger menu and rwd styles

--- index.js
+++ index.js
@@ -55,9 +55,58 @@ const toggleDropdownMenu = () => {
 			} else {
 				button.setAttribute( 'aria-expanded', 'false' );
 			}
-		})
+		} )
+	}
+}
+
+/**
+ * Toggle hamburger menu and submenu by item click
+ */
+
+const toggleMobileMenu = () => {
+
+	const hamburger = document.querySelector( '.top-bar__hamburger' );
+	const menu = document.querySelector( '.top-bar nav' );
+	const menuItems = document.querySelectorAll( '.top-bar__menu-item--has-children' );
+
+	if ( hamburger && menu ) {
+
+		hamburger.addEventListener( 'click', () => {
+			menu.style.display = 'block' !== menu.style.display ? 'block' : 'none';
+			if ( 'false' === hamburger.getAttribute( 'aria-expanded' ) ) {
+				hamburger.setAttribute( 'aria-expanded', 'true' );
+			} else {
+				hamburger.setAttribute( 'aria-expanded', 'false' );
+			}
+
+			for ( const item of menuItems ) {
+				item.classList.toggle( 'top-bar__menu-item--has-children-mobile' );
+			}
+			
+			const mobileMenuItems = document.querySelectorAll( '.top-bar__menu-item--has-children-mobile' );
+
+			for ( const item of mobileMenuItems ) {
+				const submenu = item.querySelector( '.top-bar__submenu' );
+				const link = item.getElementsByTagName( 'a' )[0];
+
+				link.setAttribute( 'aria-haspopup', 'true' );
+
+				item.addEventListener( 'click', () => {
+					submenu.classList.toggle( 'top-bar__submenu--visible' );
+
+					if ( 'false' === link.getAttribute( 'aria-expanded' ) ) {
+						link.setAttribute( 'aria-expanded', 'true' );
+					} else {
+						link.setAttribute( 'aria-expanded', 'false' );
+					}
+				} )
+			}
+		} )
+
+
 	}
 }
 
 window.addEventListener( 'DOMContentLoaded', displayGalleryItem );
 window.addEventListener( 'DOMContentLoaded', toggleDropdownMenu );
+window.addEventListener( 'DOMContentLoaded', toggleMobileMenu );
